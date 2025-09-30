07a626d377aea7af92d3acd4079a89f8883584c3
nuzulul <nuzulul@users.noreply.github.com>
2025-09-30 14:28:51 +0700

    initial code

---
+++ js/app.js
@@ -0,0 +1,28 @@
+var $ = Dom7;
+
+
+var app = new Framework7({
+  name: 'MyLMC', // App name
+  theme: 'auto', // Automatic theme detection
+  colors: {
+    primary: '#007aff',
+  },
+
+  el: '#app', // App root element
+
+  // App store
+  store: store,
+  // App routes
+  routes: routes,
+});
+// Login Screen Demo
+$('#my-login-screen .login-button').on('click', function () {
+  var username = $('#my-login-screen [name="username"]').val();
+  var password = $('#my-login-screen [name="password"]').val();
+
+  // Close login screen
+  app.loginScreen.close('#my-login-screen');
+
+  // Alert username and password
+  app.dialog.alert('Username: ' + username + '<br/>Password: ' + password);
+});
